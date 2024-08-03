c005498ec94f5b0d43d1995dc607491c2b8a809b
shahed141123 <khandkershahed23@gmail.com>
2024-08-03 13:01:54 +0600

    27-07-24

--- app/Http/Controllers/Admin/BlogCategoryController.php
+++ app/Http/Controllers/Admin/BlogCategoryController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\BlogCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class BlogCategoryController extends Controller
 {
@@ -28,7 +33,60 @@ class BlogCategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        DB::beginTransaction();
+
+        try {
+            $categoryId = $this->route('category') ?? null;
+            $validator = Validator::make($request->all(), [
+                'name'         => [
+                    'required',
+                    'string',
+                    'max:255',
+                    Rule::unique('blog_categories', 'name')->ignore($categoryId),
+                ],
+                'image'       => 'nullable|string',
+                'meta_title'  => 'nullable|string|max:255',
+                'description' => 'nullable|string',
+                'status'      => 'required|in:active,inactive',
+            ], [
+                'name.required'       => 'The name field is required.',
+                'name.string'         => 'The name must be a string.',
+                'name.max'            => 'The name may not be greater than :max characters.',
+                'name.unique'         => 'This name has already been taken.',
+                'image.string'        => 'The image must be a string.',
+                'meta_title.string'   => 'The meta title must be a string.',
+                'meta_title.max'      => 'The meta title may not be greater than :max characters.',
+                'description.string'  => 'The description must be a string.',
+                'status.required'     => 'The status field is required.',
+                'status.in'           => 'The status must be one of: active, inactive.',
+            ]);
+
+            if ($validator->fails()) {
+                foreach ($validator->messages()->all() as $message) {
+                    Session::flash('error', $message);
+                }
+                return redirect()->back()->withInput();
+            }
+
+            BlogCategory::create([
+                'name'        => $request->name,
+                'slug'        => $request->slug,
+                'image'       => $request->image,
+                'meta_title'  => $request->meta_title,
+                'description' => $request->description,
+                'status'      => $request->status,
+            ]);
+
+            // toastr()->success('Data has been saved successfully!');
+            return redirect()->back()->with('success', 'Data has been saved successfully!');
+        } catch (\Exception $e) {
+            // Rollback the database transaction in case of an error
+            DB::rollback();
+
+            // Return back with error message
+            return redirect()->back()->withInput()->with('error', 'An error occurred while creating the Category: ' . $e->getMessage());
+        }
     }
 
     /**
--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class BlogCategory extends Model
 {
     use HasFactory;
-
+    protected $slugSourceColumn = 'name';
     /**
      * The attributes that aren't mass assignable.
      *
