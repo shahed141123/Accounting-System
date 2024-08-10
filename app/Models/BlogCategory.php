1460215cd0feaaffe2cb71ad9fcc51e22eef5beb
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-08-10 23:55:02 +0600

    10-08-24

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BlogCategory extends Model
 {
-    use HasFactory;
+    use HasFactory,HasSlug;
+
     protected $slugSourceColumn = 'name';
     /**
      * The attributes that aren't mass assignable.
--- app/Models/BlogTag.php
+++ app/Models/BlogTag.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BlogTag extends Model
 {
-    use HasFactory;
+    use HasFactory,HasSlug;
     protected $slugSourceColumn = 'name';
     /**
      * The attributes that aren't mass assignable.
