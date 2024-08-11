173684cb4e3dbb2e241f211f493102591bfafaf1
shahed141123 <khandkershahed23@gmail.com>
2024-08-11 11:00:36 +0600

    07-08-24

---
+++ app/Http/Controllers/User/ClientController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    public function orderHistory()
+    {
+        return view('user.pages.orderHistory');
+    }
+    public function accountDetails()
+    {
+        return view('user.pages.accountDetails');
+    }
+    public function quickOrder()
+    {
+        return view('user.pages.quickOrder');
+    }
+    public function stockHistory()
+    {
+        return view('user.pages.stockHistory');
+    }
+    public function wishlist()
+    {
+        return view('user.pages.wishlist');
+    }
+}
