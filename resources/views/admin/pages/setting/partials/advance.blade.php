5ec34312c1ce548ab24947e4b900ab9d6150a652
Sazeduzzaman-Saju <szamansaju@gmail.com>
2024-09-01 23:17:35 +0600

    Some change done

--- resources/views/admin/pages/setting/partials/advance.blade.php
+++ resources/views/admin/pages/setting/partials/advance.blade.php
@@ -1,6 +1,26 @@
-Need Check Box
-    1. User Verification Cheking(user_verification)(Value 1)
-
-
-And Need One Input Field
-=== minimum_order_quantity() "Number"
+<div class="row">
+    <div class="col-xl-12">
+        <h1>Change Order Quantity</h1>
+    </div>
+    <div class="col-xl-12">
+        <div class="row mt-10 px-5 align-items-center bg-light w-75 rounded-3">
+            <div class="col-xl-4">
+                <p class="fw-bold">Verification</p>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="1" name="user_verification"
+                        id="flexCheckChecked" />
+                    <label class="form-check-label" for="flexCheckChecked">
+                        User Verification Cheking
+                    </label>
+                </div>
+            </div>
+            <div class="col-xl-8 mt-5">
+                <div class="mb-10">
+                    <label for="exampleFormControlInput1" class="required form-label">Add Minimum Order Quantity</label>
+                    <input type="numnber" name="minimum_order_quantity" class="form-control"
+                        placeholder="User Minimum Order Quantity" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
