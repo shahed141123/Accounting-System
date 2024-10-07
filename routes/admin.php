20c99c14ec3546cf5f604f3fd20fef3cf7446d55
shahed141123 <khandkershahed23@gmail.com>
2024-10-07 16:36:33 +0600

    31-08-24

--- routes/admin.php
+++ routes/admin.php
@@ -65,32 +65,24 @@ Route::get('/', function () {
     return redirect()->route('admin.login');
 })->name('home');
 
-// Route::middleware('guest:admin')->name('admin.')->group(function () {
-
-//     Route::get('/', [AuthenticatedSessionController::class, 'create'])
-//         ->name('login');
-
-//     Route::post('login', [AuthenticatedSessionController::class, 'store']);
-
-//     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-//         ->name('password.request');
-
-//     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-//         ->name('password.email');
-
-//     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-//         ->name('password.reset');
-
-//     Route::post('reset-password', [NewPasswordController::class, 'store'])
-//         ->name('password.store');
-// });
 Route::middleware('guest:admin')->name('admin.')->group(function () {
-    Route::get('/', [AuthenticatedSessionController::class, 'create'])->name('login');
+
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
+
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
-    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+        ->name('password.request');
+
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->name('password.email');
+
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+        ->name('password.reset');
+
+    Route::post('reset-password', [NewPasswordController::class, 'store'])
+        ->name('password.store');
 });
 
 // Route::get('/dashboard', [AdminController::class, 'dashboard'])
