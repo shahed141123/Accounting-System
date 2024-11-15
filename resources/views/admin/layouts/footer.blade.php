cae566cb17ab71b28544c43845e87c97f1e5b1c1
Sazeduzzaman-Saju <szamansaju@gmail.com>
2024-11-15 21:40:30 +0600

    Logout Button Done

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,6 +1,5 @@
 <footer class="app-footer">
     <span>
-        Copyright &copy; Flixa Global 2024&nbsp;
         <a href="http://{{ optional($setting)->copyright_url }}"
             class="text-decoration-none">{{ optional($setting)->copyright_title ?? optional($setting)->site_title }}</a>.
     </span>
